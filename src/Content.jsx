176af3c1b16d3cc8d3bcae3ce8c624a41c6a54b8
xsfxtsxxr <xusongfu5050@163.com>
2021-04-09 18:37:06 +0800

    add yaml-js

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,9 +2,22 @@ import React, { useState, useEffect } from 'react'
 import G6 from '@antv/g6'
 import './Content.css'
 import { v4 as uuidv4 } from 'uuid'
-import { Input, Button, message, Typography } from 'antd'
-
+import { Input, Button, message, Typography, Radio } from 'antd'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/mode/yaml/yaml.js'
 const { Paragraph } = Typography
+const { TextArea } = Input
+let content = `heat_template_version: 2018-08-31
+parameters:
+  image:
+    type: string
+    constraints:
+      - custom_constraint: glance.image
+  flavor:
+    type: string`
+const yaml = require('js-yaml')
+
 function onDragOver (e) {
   e.preventDefault()
 }
@@ -17,6 +30,8 @@ function Content ({ item }) {
   const [edges, setEdges] = useState([])
   const [selectItem, setItem] = useState({})
   const [target, setTarget] = useState(null)
+  const [text, setText] = useState(content)
+  const [value, setValue] = useState('yaml')
   useEffect(() => {
     if (!graph) {
       graph = new G6.Graph({
@@ -24,42 +39,31 @@ function Content ({ item }) {
         width: ref.current.scrollWidth,
         height: ref.current.scrollHeight || 500,
         minZoom: 0.1,
-        fitCenter: true,
+        // fitCenter: true,
         modes: {
           default: [
             {
               type: 'drag-canvas',
               enableOptimize: true,
-            },
-            {
+            }, {
               type: 'zoom-canvas',
-              enableOptimize: true,
-              optimizeZoom: 0.5,
-            }
-          ],
-          edit: ['click-select']
+              minZoom: 0.5,
+              maxZoom: 2
+            }, {
+              type: 'drag-node',
+            }],
+          // edit: ['click-select']
         },
         layout: {
-          type: 'dagre',
-          rankdir: 'LR',
-          nodesep: 10,
-          ranksep: 50,
+          type: 'force',
+          preventOverlap: true,
+          nodeSpacing: 50
         },
         defaultNode: {
-          type: 'rect',
-          labelCfg: {
-            style: {
-              fill: '#f00',
-              // fontSize: 10,
-            },
-          },
-          style: {
-            stroke: '#72CC4A',
-            width: 150,
-          }
+          type: 'ellipse',
         },
         defaultEdge: {
-          type: 'fund-polyline',
+          type: 'cubic',
         },
       })
     }
@@ -77,7 +81,7 @@ function Content ({ item }) {
 
   function onDrop (e) {
     cacheNodes.push({
-      ...item, id: uuidv4(), type: 'ellipse'
+      ...item, id: uuidv4()
     })
     setNodes([...cacheNodes])
     createEdges()
@@ -87,18 +91,22 @@ function Content ({ item }) {
     cacheEdges = cacheNodes.filter(elem => elem.target)
     const newEdges = []
     cacheEdges.forEach(elem => {
-      let target = elem.target && elem.target.split(',')
+      let target = elem.target && elem.target.split(/,|，|\s/)
       if (target) {
         const subs = target.map(sub => ({
           source: elem.id,
           target: sub,
           style: {
-            endArrow: true,
+            endArrow: {
+              path: G6.Arrow.vee(10, 20, 10),
+              d: 15
+            },
           }
         }))
         newEdges.push(...subs)
       }
     })
+    console.log(cacheNodes, newEdges)
     setEdges([...newEdges])
   }
 
@@ -114,13 +122,40 @@ function Content ({ item }) {
     })
     setNodes([...cacheNodes])
     createEdges()
-    // 当前数据
-    // console.log(cacheNodes, cacheEdges)
   }
+
+  function onChange (e) {
+    setValue(e.target.value)
+    content = e.target.value
+    // let isYaml = !!yaml.load(text)
+    // console.log(isYaml)
+    if (e.target.value === 'javascript') {
+      let result = yaml.load(text)
+      setText(JSON.stringify(result, null, 2))
+    } else {
+      let result = yaml.dump(JSON.parse(text))
+      setText(result)
+    }
+  };
 
   return (
     <div className="content">
       <div ref={ref} className="canvas" onDrop={onDrop} onDragOver={onDragOver}></div>
+      <Radio.Group onChange={onChange} value={value}>
+        <Radio value='javascript'>JSON</Radio>
+        <Radio value='yaml'>YAML</Radio>
+      </Radio.Group>
+      <CodeMirror
+        value={text}
+        options={{
+          mode: { name: value },
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setText(value)
+        }}
+      />
       {
         selectItem.id && <div className="box">
           <Paragraph
@@ -130,9 +165,9 @@ function Content ({ item }) {
             className="paragraph">
             ID: {selectItem.id}
           </Paragraph>
-          <Input placeholder="名称" value={selectItem.label} disabled />
-          <Input placeholder="类型" value={selectItem.value} disabled />
-          <Input placeholder="目标" value={target} onChange={e => (setTarget(e.target.value))} />
+          <Input className="theInput" placeholder="名称" value={selectItem.label} disabled />
+          <Input className="theInput" placeholder="类型" value={selectItem.value} disabled />
+          <TextArea className="theInput" placeholder="目标" value={target} onChange={e => (setTarget(e.target.value))} />
           <Button type="primary" onClick={handleClick.bind(this, selectItem.id)}>编辑节点关系</Button>
         </div>
       }
