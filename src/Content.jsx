a154bcbb36be4dab999991dfcd79383e2c1aa529
xsfxtsxxr <xusongfu5050@163.com>
2021-04-12 18:19:20 +0800

    fix click bug

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,36 +2,26 @@ import React, { useState, useEffect } from 'react'
 import G6 from '@antv/g6'
 import './Content.css'
 import { v4 as uuidv4 } from 'uuid'
-import { Input, Button, message, Typography, Radio } from 'antd'
+import { Button, message, Radio, Tabs } from 'antd'
 import { Controlled as CodeMirror } from 'react-codemirror2'
 import 'codemirror/mode/javascript/javascript.js'
 import 'codemirror/mode/yaml/yaml.js'
-const { Paragraph } = Typography
-const { TextArea } = Input
-let content = `heat_template_version: 2018-08-31
-parameters:
-  image:
-    type: string
-    constraints:
-      - custom_constraint: glance.image
-  flavor:
-    type: string`
+const { TabPane } = Tabs
 const yaml = require('js-yaml')
 
 function onDragOver (e) {
-  e.preventDefault()
+  e.stopPropagation()
 }
 
 let graph = null, cacheNodes = [], cacheEdges = []
-function Content ({ item }) {
+export default function Content ({ item }) {
   const ref = React.useRef(null)
   // 定义数据源
   const [nodes, setNodes] = useState([])
   const [edges, setEdges] = useState([])
-  const [selectItem, setItem] = useState({})
-  const [target, setTarget] = useState(null)
-  const [text, setText] = useState(content)
-  const [value, setValue] = useState('yaml')
+  const [templateValue, setTemplateValue] = useState('') // Template value
+  const [value, setValue] = useState('yaml') // CodeMirror mode value
+  const [component, setComponent] = useState('') // Component value
   useEffect(() => {
     if (!graph) {
       graph = new G6.Graph({
@@ -39,7 +29,7 @@ function Content ({ item }) {
         width: ref.current.scrollWidth,
         height: ref.current.scrollHeight || 500,
         minZoom: 0.1,
-        // fitCenter: true,
+        fitCenter: true,
         modes: {
           default: [
             {
@@ -67,17 +57,38 @@ function Content ({ item }) {
         },
       })
     }
-    graph.on('click', evt => {
-      evt.stopPropagation()
-      if (evt.item) {
-        const { id, value, label, target } = evt.item._cfg.model
-        setItem({ id, value, label, target })
-        setTarget(target)
-      }
-    })
     graph.data({ nodes: nodes, edges: edges })
     graph.render()
   }, [nodes, edges])
+
+  useEffect(() => {
+    let cachedItem = null
+    graph.on('node:click', evt => {
+      evt.stopPropagation()
+      const yamlText = jsToYaml(evt.item._cfg.model)
+      const jsText = yamlToJs(yamlText)
+      setComponent(jsText)
+      // if cachedItem, will create line for the two nodes(except cachedTtem)
+      if (cachedItem && cachedItem.id !== evt.item._cfg.model.id) {
+        cacheNodes.forEach(elem => {
+          if (elem.id === cachedItem.id) {
+            if (elem.target) {
+              elem.target += `,${evt.item._cfg.model.id}`
+            } else {
+              elem.target = evt.item._cfg.model.id
+            }
+          }
+        })
+        setNodes([...cacheNodes])
+        createEdges()
+        cachedItem = null
+      }
+    })
+    graph.on("node:dblclick", evt => {
+      evt.stopPropagation()
+      cachedItem = evt.item._cfg.model
+    })
+  }, [])
 
   function onDrop (e) {
     cacheNodes.push({
@@ -106,57 +117,97 @@ function Content ({ item }) {
         newEdges.push(...subs)
       }
     })
-    console.log(cacheNodes, newEdges)
     setEdges([...newEdges])
   }
 
-  function handleClick (id) {
-    if (!id || !target) {
-      return message.error('请选择节点')
-    }
-    cacheNodes = [...nodes]
-    cacheNodes.forEach(elem => {
-      if (elem.id === id) {
-        elem.target = target
-      }
-    })
-    setNodes([...cacheNodes])
-    createEdges()
+  // save template
+  function saveTemplate () {
+    message.error('开发中...')
   }
 
+  function saveResource () {
+    message.error('开发中...')
+  }
+
+  // radio group change
   function onChange (e) {
     setValue(e.target.value)
-    content = e.target.value
-    // let isYaml = !!yaml.load(text)
-    // console.log(isYaml)
     if (e.target.value === 'javascript') {
-      let result = yaml.load(text)
-      setText(JSON.stringify(result, null, 2))
+      let result = yamlToJs(templateValue)
+      setTemplateValue(result)
     } else {
-      let result = yaml.dump(JSON.parse(text))
-      setText(result)
+      let result = jsToYaml(templateValue)
+      setTemplateValue(result)
     }
-  };
+  }
 
+  // tab change
+  function callback (key) {
+    console.log(key, edges, nodes)
+    // change to template tab, reset template value
+    if (key === 'template') {
+      if (value === 'yaml') {
+        console.log('要转为yaml')
+        setTemplateValue(jsToYaml(nodes))
+      } else {
+        console.log('要转为yaml')
+        const yamlText = jsToYaml(nodes)
+        const jsText = yamlToJs(yamlText)
+        setTemplateValue(jsText)
+      }
+    }
+  }
+
+  // yaml格式转js
+  function yamlToJs (text) {
+    let result = yaml.load(text)
+    return JSON.stringify(result, null, 2)
+  }
+  // js格式转yaml
+  function jsToYaml (text) {
+    let result = yaml.dump(typeof text !== 'object' ? JSON.parse(text) : text)
+    return result
+  }
   return (
     <div className="content">
       <div ref={ref} className="canvas" onDrop={onDrop} onDragOver={onDragOver}></div>
-      <Radio.Group onChange={onChange} value={value}>
-        <Radio value='javascript'>JSON</Radio>
-        <Radio value='yaml'>YAML</Radio>
-      </Radio.Group>
-      <CodeMirror
-        value={text}
-        options={{
-          mode: { name: value },
-          theme: 'material',
-          lineNumbers: true
-        }}
-        onBeforeChange={(editor, data, value) => {
-          setText(value)
-        }}
-      />
-      {
+      <Tabs onChange={callback} type="card">
+        <TabPane tab="Component" key="component">
+          <div style={{ color: '#fff' }}>
+            <Button type="primary" size="small" onClick={saveResource}>编辑资源</Button>
+            <CodeMirror
+              value={component}
+              options={{
+                mode: { name: 'javascript' },
+                theme: 'material',
+                lineNumbers: true
+              }}
+              onBeforeChange={(editor, data, value) => {
+                setComponent(value)
+              }}
+            />
+          </div>
+        </TabPane>
+        <TabPane tab="Template" key="template">
+          <Radio.Group onChange={onChange} value={value}>
+            <Radio value='javascript'>JSON</Radio>
+            <Radio value='yaml'>YAML</Radio>
+          </Radio.Group>
+          <Button type="primary" size="small" onClick={saveTemplate}>保存模板</Button>
+          <CodeMirror
+            value={templateValue}
+            options={{
+              mode: { name: value },
+              theme: 'material',
+              lineNumbers: true
+            }}
+            onBeforeChange={(editor, data, value) => {
+              setTemplateValue(value)
+            }}
+          />
+        </TabPane>
+      </Tabs>
+      {/* {
         selectItem.id && <div className="box">
           <Paragraph
             copyable={{
@@ -170,9 +221,7 @@ function Content ({ item }) {
           <TextArea className="theInput" placeholder="目标" value={target} onChange={e => (setTarget(e.target.value))} />
           <Button type="primary" onClick={handleClick.bind(this, selectItem.id)}>编辑节点关系</Button>
         </div>
-      }
+      } */}
     </div >
   )
 }
-
-export default Content
